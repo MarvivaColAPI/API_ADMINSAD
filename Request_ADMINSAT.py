d091b61ad2990386931cf437ee26436641d2ee9a
MarvivaColAPI <api.marvivacolombia@gmail.com>
2024-12-04 11:28:21 -0500

    Update Request_ADMINSAT.py

--- Request_ADMINSAT.py
+++ Request_ADMINSAT.py
@@ -3,10 +3,10 @@ import requests
 import geojson
 import os
 from git import Repo
+from datetime import datetime
 
 # URL para solicitar el token de autenticación
 url_autenticacion = "https://api.adminsat.com/v2/api/applications/autenticacion/token/"
-# URL para solicitar la ubicación de los activos
 url_ubicacion = "https://api.adminsat.com/v2/api/applications/public/ubicacion/"
 
 # Credenciales de usuario (proporcionadas por el cliente)
@@ -32,20 +32,19 @@ cabecera_autenticacion = {
 # Hacer una solicitud POST para obtener el token de autenticación
 respuesta_autenticacion = requests.post(url_autenticacion, json=cuerpo_peticion_autenticacion, headers=cabecera_autenticacion)
 
-# Verificar si la solicitud fue exitosa (código de estado 200)
+# Verificar si la solicitud fue exitosa
 if respuesta_autenticacion.status_code == 200:
-    # Extraer el token de autenticación de la respuesta JSON
+    # Extraer el token de autenticación
     access_token = respuesta_autenticacion.json()["access_token"]
-    # Cabecera de la petición para obtener la ubicación de los activos
     cabecera_ubicacion = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {access_token}"
     }
+    
     # Hacer una solicitud GET para obtener la ubicación de los activos
     respuesta_ubicacion = requests.get(url_ubicacion, headers=cabecera_ubicacion)
-    # Verificar si la solicitud fue exitosa (código de estado 200)
+    
     if respuesta_ubicacion.status_code == 200:
-        # Extraer la información de ubicación de la respuesta JSON
         ubicaciones = respuesta_ubicacion.json()
         
         # Crear una lista para almacenar las características GeoJSON
@@ -60,6 +59,10 @@ if respuesta_autenticacion.status_code == 200:
         
         # Iterar sobre cada ubicación y crear una característica GeoJSON para cada una
         for ubicacion in ubicaciones:
+            # Convertir las fechas de UNIX a formato americano
+            fecha_hora_equipo = datetime.fromtimestamp(ubicacion["fecha_hora_equipo"]).strftime('%m/%d/%Y %H:%M:%S')
+            fecha_hora_servidor = datetime.fromtimestamp(ubicacion["fecha_hora_servidor"]).strftime('%m/%d/%Y %H:%M:%S')
+            
             # Crear un diccionario de propiedades para la ubicación
             propiedades = {
                 "Nombre": ubicacion["nombre"],
@@ -69,20 +72,18 @@ if respuesta_autenticacion.status_code == 200:
                 "Ignición": "Encendido" if ubicacion["ignicion"] else "Apagado" if ubicacion["ignicion"] is not None else "Desconocido",
                 "Porcentaje de batería": ubicacion["bateria"],
                 "Altitud": ubicacion["altitud"],
-                "Fecha y hora del equipo": ubicacion["fecha_hora_equipo"],
-                "Fecha y hora del servidor": ubicacion["fecha_hora_servidor"],
+                "Fecha y hora del equipo": fecha_hora_equipo,
+                "Fecha y hora del servidor": fecha_hora_servidor,
                 "Evento": ubicacion["evento_nombre"],
                 "Identificador": ubicacion["identificador"],
                 "Odómetro": ubicacion["odometro"]
             }
             
-            # Crear una geometría Point usando las coordenadas de latitud y longitud
+            # Crear una geometría Point usando las coordenadas
             geometria = geojson.Point((ubicacion["longitud"], ubicacion["latitud"]))
             
-            # Crear una característica GeoJSON con las propiedades y la geometría
+            # Crear una característica GeoJSON
             feature = geojson.Feature(geometry=geometria, properties=propiedades)
-            
-            # Agregar la característica a la lista de características
             features.append(feature)
         
         # Crear una colección FeatureCollection con todas las características
@@ -91,23 +92,16 @@ if respuesta_autenticacion.status_code == 200:
         # Escribir la colección FeatureCollection en el archivo .geojson
         with open(archivo_geojson, "w") as archivo:
             geojson.dump(feature_collection, archivo, indent=4)
-            
+        
         print("Archivo .geojson creado correctamente.")
         
         # Agregar y hacer commit de los cambios en el repositorio de GitHub
         repo_dir = os.path.abspath(os.path.dirname(__file__))  # Ruta al directorio del script
         repo = Repo(repo_dir)
-        
-        # Agregar los cambios
         repo.git.add(archivo_geojson)
-        
-        # Hacer commit de los cambios
         repo.index.commit('Actualización automática del archivo ubicaciones.geojson')
-        
-        # Empujar los cambios al repositorio remoto
         origin = repo.remote(name='origin')
         origin.push()
-            
     else:
         print("Error al obtener la ubicación de los activos:", respuesta_ubicacion.status_code)
 else:
